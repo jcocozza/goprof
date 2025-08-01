503d0279538816dc4bc271c6af8ada2c040ef813
Joseph Cocozza <jcocozza47@gmail.com>
2025-08-01 08:25:27 -0400

    feat: add a run method and more comments

--- profiler.go
+++ profiler.go
@@ -1,6 +1,35 @@
+/*
+goprof is a convenience wrapper around go's pprof library.
+If you need control profiling, don't use this.
+There are three main ways to use this package.
+
+1. Targets to a specific bit of code
+
+	goprof.Run("<name>", func() {
+		<your arbitrary code>
+	})
+
+2. Target a chunk of code. Anywhere in your code call the following:
+
+	if err := goprof.Start("<name>"); err != nil {
+		// handle error
+	}
+
+<your code here>
+
+	if err := goprof.End(); err != nil {
+		// handle error
+	}
+
+3. Profile all your code. At the beginning of your main method, just call:
+
+goprof.Start("<name>")
+defer goprof.End()
+*/
 package goprof
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"runtime"
@@ -17,6 +46,16 @@ type profiler struct {
 	cpu   *os.File
 	block *os.File
 	trace *os.File
+	heap  *os.File
+}
+
+var ErrAlreadyStarted = errors.New("profiler already started")
+
+func (p *profiler) started() error {
+	if !p.start.IsZero() && !p.end.IsZero() {
+		return ErrAlreadyStarted
+	}
+	return nil
 }
 
 func (p *profiler) duration() time.Duration {
@@ -43,6 +82,12 @@ func setupFiles(name string) error {
 		return err
 	}
 	p.trace = trace
+
+	heap, err := os.Create(fmt.Sprintf("%s.heap.prof", name))
+	if err != nil {
+		return err
+	}
+	p.heap = heap
 	return nil
 }
 
@@ -56,10 +101,22 @@ func cleanupFiles() error {
 	if err := p.trace.Close(); err != nil {
 		return err
 	}
+	if err := p.heap.Close(); err != nil {
+		return err
+	}
 	return nil
 }
 
+// name is optional; if name is an empty string, will populate with a time stamp
 func Start(name string) error {
+	if err := p.started(); err != nil {
+		return err
+	}
+
+	if name == "" {
+		name = fmt.Sprintf("goprof-%d", time.Now().UnixNano())
+	}
+
 	if err := setupFiles(name); err != nil {
 		return err
 	}
@@ -80,11 +137,14 @@ func Start(name string) error {
 }
 
 func Stop() error {
-	// run this first; we don't want teardown to affect total time
+	// run this first; we don't want tear down to affect total time
 	p.end = time.Now()
 	pprof.StopCPUProfile()
 	trace.Stop()
 	if err := pprof.Lookup("block").WriteTo(p.block, 0); err != nil {
+		return err
+	}
+	if err := pprof.WriteHeapProfile(p.heap); err != nil {
 		return err
 	}
 
@@ -92,6 +152,15 @@ func Stop() error {
 		return err
 	}
 	return nil
+}
+
+// convenience wrapper to profile an arbitrary function
+func Run(name string, f func()) error {
+	if err := Start(name); err != nil {
+		return err
+	}
+	f()
+	return Stop()
 }
 
 // summary functions
